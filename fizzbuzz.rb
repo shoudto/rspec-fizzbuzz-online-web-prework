18d0b07d5d95f46686456f97ca563f76cba4774f
Oscar Shoudt <shoudto@gmail.com>
2019-03-23 18:49:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 
-    "Fizz"
+  if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
     elsif
       int % 5 == 0
       "Buzz"
       elsif 
-       int % 5 == 0 && int % 3  == 0
-      "FizzBuzz"
+       int % 3 == 0 
+      "Fizz"
   end
 end 
